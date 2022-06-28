30b9b9a03711454ecfdb8270c24ba0a7cb19dc66
Seremet Alexandru <andutzu.268@gmail.com>
2022-06-28 22:24:10 +0300

    made async component work

--- src/app/async/async.component.ts
+++ src/app/async/async.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Area, Thing } from '../shared/interfaces';
+import { Observable } from 'rxjs';
+import { DataService } from '../shared/services/data.service';
 
 @Component({
   selector: 'app-async',
@@ -6,4 +9,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./async.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AsyncComponent {}
+export class AsyncComponent implements OnInit {
+  areas$!: Observable<Area[]>;
+  things$!: Observable<Thing[]>;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.areas$ = this.dataService.getAreas();
+
+    this.things$ = this.dataService.getThings();
+  }
+}
--- src/app/async/async.module.ts
+++ src/app/async/async.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AsyncComponent } from './async.component';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AsyncComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
   exports: [RouterModule],
 })
 export class AsyncModule {}
